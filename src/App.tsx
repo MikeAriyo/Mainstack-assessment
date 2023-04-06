369bdbcb8d003c05dff096bf53fc2fbadf85b0f2
MikeAriyo <ariyoadekola2@gmail.com>
2023-04-06 18:13:05 +0100

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-red-500">Test Work</h2>
+    </div>
+  );
+}
+
+export default App;
