5b4a38d422a850cb7f33166ac2f55eddc71f810a
MikeAriyo <ariyoadekola2@gmail.com>
2023-07-05 08:37:17 +0100

    made greeting text dynamic

--- app/components/dashboard/dashboardGreeting.tsx
+++ app/components/dashboard/dashboardGreeting.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 
 const DashboardGreeting = () => {
+  const time = new Date().getHours();
+  let greeting;
+  if (time < 12) {
+    greeting = "Good Morning";
+  } else if (time < 18) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
   return (
     <>
       <main className="md:flex justify-between items-center min-w-[100%]  p-4 pl-0">
         <div className="">
           <h2 className="font-bold text-[24px] mb-2 text-[#131316]">
-            Good morning, Blessing ⛅️
+            {greeting}, Blessing ⛅️
           </h2>
           <p className="text-[#31373D]">Check out your dashboard summary.</p>
         </div>
